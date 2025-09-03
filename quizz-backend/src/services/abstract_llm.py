afb5e06dcb408deec9e637f1b53e48ab36f4f6e5
waseem-code7 <mohammedwaseem2601@gmail.com>
2025-09-04 00:15:48 +0530

    added retry parser wrapper

--- quizz-backend/src/services/abstract_llm.py
+++ quizz-backend/src/services/abstract_llm.py
@@ -1,10 +1,13 @@
 from abc import ABC, abstractmethod
 
+from langchain.output_parsers import RetryWithErrorOutputParser
+
 
 class AbstractBaseLLMRequest(ABC):
     def __init__(self, model, parser):
         self.parser = parser
         self.model = model
+        self.retry_parser = RetryWithErrorOutputParser.from_llm(parser=parser, llm=self.model, max_retries=3)
 
 
     @abstractmethod
